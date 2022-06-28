b7417644dbf3ac8c699f10e3c2d7d14890306918
saff-it <saff_it@ymail.com>
2022-06-28 15:14:04 +0200

    Milestone 0 completed - array set up

---
+++ script.js
@@ -0,0 +1,34 @@
+
+const peopleArray = [
+    {
+        nome : 'Wayne Barnett',
+        mansione : 'Founder & CEO',
+        image : 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        nome : 'Angela Caroll',
+        mansione : 'Chief Editor',
+        image : 'angela-caroll-chief-editor.jpg',
+    },
+    {
+        nome : 'Walter Gordon',
+        mansione : 'Office Manager',
+        image : 'walter-gordon-office-manager.jpg',
+    },
+    {
+        nome : 'Angela Lopez',
+        mansione : 'Social Media Manager',
+        image : 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        nome : 'Scott Estrada',
+        mansione : 'Developer',
+        image : 'scott-estrada-developer.jpg',
+    },
+    {
+        nome : 'Barbara Ramos',
+        mansione : 'Graphic Designer',
+        image : 'barbara-ramos-graphic-designer.jpg',
+    },
+
+];
