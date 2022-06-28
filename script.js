fb850459ef8391fafa277fccd76e4f9ff8fb4b8b
saff-it <saff_it@ymail.com>
2022-06-28 16:16:21 +0200

    Bonus 1 - printing pics on dom attempting

--- script.js
+++ script.js
@@ -3,32 +3,32 @@ const peopleArray = [
     {
         nome : 'Wayne Barnett',
         mansione : 'Founder & CEO',
-        image : 'wayne-barnett-founder-ceo.jpg',
+        image : '../img/wayne-barnett-founder-ceo.jpg',
     },
     {
         nome : 'Angela Caroll',
         mansione : 'Chief Editor',
-        image : 'angela-caroll-chief-editor.jpg',
+        image : '../img/angela-caroll-chief-editor.jpg',
     },
     {
         nome : 'Walter Gordon',
         mansione : 'Office Manager',
-        image : 'walter-gordon-office-manager.jpg',
+        image : '../img/walter-gordon-office-manager.jpg',
     },
     {
         nome : 'Angela Lopez',
         mansione : 'Social Media Manager',
-        image : 'angela-lopez-social-media-manager.jpg',
+        image : '../img/angela-lopez-social-media-manager.jpg',
     },
     {
         nome : 'Scott Estrada',
         mansione : 'Developer',
-        image : 'scott-estrada-developer.jpg',
+        image : '../img/scott-estrada-developer.jpg',
     },
     {
         nome : 'Barbara Ramos',
         mansione : 'Graphic Designer',
-        image : 'barbara-ramos-graphic-designer.jpg',
+        image : '../img/barbara-ramos-graphic-designer.jpg',
     },
 
 ];
@@ -37,34 +37,28 @@ const peopleArray = [
 //     console.log(peopleArray[key]);
 // }
 
-const cardWrapper = document.getElementById('row-wrap');
 
+const cardWrapper = document.getElementById('row-wrap');
 
 for ( let i = 0; i < peopleArray.length; i++){
 
     let domCard = document.createElement('div');
     domCard.classList.add('col-4');
-
+    
     domCard.innerHTML = `<div class="card">
-        <img src="" class="card-img-top">
+        <img src="..." class="card-img-top">
         <div class="card-body">
             <h5 class="card-name">${peopleArray[i].nome}</h5>
             <p class="card-text">${peopleArray[i].mansione}</p>
         </div>
     </div>`;
 
+    const cardImg = document.querySelector('img.card-img-top');
+    
+
+
     cardWrapper.append(domCard);
-
     console.log(`${peopleArray[i].nome} ${peopleArray[i].mansione} ${peopleArray[i].image}`);
+    
+
 };
-
-
-/* <div class="col-4">
-                    <div class="card">
-                        <img src="" class="card-img-top">
-                        <div class="card-body">
-                          <h5 class="card-name">Name</h5>
-                          <p class="card-text">Job position</p>
-                        </div>
-                      </div>
-                </div> */
